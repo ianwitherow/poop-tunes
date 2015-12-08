471855c32ec5b21e80a3da2238af9831167cfa0b
Ian Witherow <ian.witherow@sourceot.com>
2015-12-08 13:26:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+PoopTunes
+=========
+
+<pre>
+                   ______
+                   \     \.
+                   |`\_____\               ____------
+                   |` |    |              ||       ||
+                   |  |    |              ||       ||
+      __-====-__  _|  |    |              ||       ||
+     (~<       >~>  \ |    |              ||      (#)
+     !~~-====-~~/----`+----/            (#)
+      \         \___     /
+       >------\     \  <
+      <_________________>
+</pre>
+
+What is it?
+-----------
+Is your bathroom at work too quiet? It's weird, right? *PoopTunes* solves that age-old problem by playing some music when the lights turn on.
+
+How do I use it?
+----------------
+* Grab a photoresistor. I found mine on [Amazon] [1], but they're also on [SparkFun] [2] or probably lots of other places.
+* Wire it up - I used this site as a guide: [https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi/basic-photocell-reading](https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi/basic-photocell-reading)
+* Make sure you have [RPi.GPIO](https://pypi.python.org/pypi/RPi.GPIO) installed
+* Install either MPG123 or MPG321. The script uses MPG123, so if you're using the other one then you'll have to get in there and change it.
+* (Optional) Check the readings for the light levels.
+  * Run the script with the --lighting flag, or -l. This will give you the light reading - find a number between the lowest and highest for the 'threshold'. This is the cutoff where the program decides if the light is on or off. For example, if you turn off the light and the reading is at 4000, then you turn on the light and it's at 200, pick somewhere inbetween like 500 or 1000.
+* Run the script with `python poop-tunes.py --threshold [number]`
+  * Threshold is optional (default is 2000). Use this if it's not accurately detecting whether the light is on or off.
+
+Dependencies
+------------
+* Python 2.x
+* RPi.GPIO
+* mpg123 or mpg321 (script uses 123, so if you use 321 you'll have to change that)
+
+
+[1] "http://www.amazon.com/Sensitive-Resistor-Photoresistor-Optoresistor-GM5539/dp/B00AQVYWA2"
+[2] "https://www.sparkfun.com/products/9088"
---
+++ poop-tunes.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+from signal import *
+from subprocess import Popen, PIPE
+import os, sys, random, time
+import RPi.GPIO as GPIO
+import argparse
+
+
+#Set arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--lighting', action='store_true', help='Run in light-reading mode')
+parser.add_argument('-t', '--threshold', help='Set the light threshold for when the music should play (use --lighting to figure out what you need).', default=2000, type=int)
+args = parser.parse_args()
+
+#Global variables
+lightOn          = False                     # Whether or not the light is on
+playing          = False                     # Whether or not a song is currently playing
+hasPlayedOnce    = False                     # Keep track of whether or not we've played a song yet so we don't overwrite the initial status message
+music            = None                      # mpg123 process that is playing a song
+song             = None                      # Name of the song playing
+songPath         = "/root/poop-tunes/music/" # Directory to grab songs from
+lastSong         = None                      # Name of the last song played
+threshold        = args.threshold            # Threshold for the photoresistor that tells us whether or not the light is on
+canceled         = False                     # Keep track of whether or not we just canceled a song. Gets reset when the light turns off.
+buttonDown       = False                     # Track button state
+buttonEventFired = False                     # For debouncing the cancel button
+poopistCount     = 0                         # Keep track of how many times the light has turned on
+
+#Set these to whichever GPIO pins you used
+BUTTON_PIN       = 17
+LIGHT_SENSOR_PIN = 18
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+#Cancel button
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+#Print a static message instead of a new line per message
+def printInline(message):
+	#Clear out what we previously wrote. TODO: keep track of length of previous message to be more precise
+	spaces = " " * 80
+	sys.stdout.write(spaces + "\r")
+	sys.stdout.write(message + "\r")
+	sys.stdout.flush()
+
+#Get the reading of the photo resistor
+def RCtime (RCpin):
+	reading = 0
+	GPIO.setup(RCpin, GPIO.OUT)
+	GPIO.output(RCpin, GPIO.LOW)
+	time.sleep(0.1)
+
+	GPIO.setup(RCpin, GPIO.IN)
+	while (GPIO.input(RCpin) == GPIO.LOW):
+		reading += 1
+	return reading
+
+#Find a new song to play
+def GetSong():
+	global lastSong
+	while True:
+		choice = random.choice(os.listdir(songPath))
+		#Don't play the same song twice in a row!
+		if (choice != lastSong):
+			lastSong = choice
+			return choice
+
+#Stops the currently playing song, if there is one.
+def StopMusic():
+	global playing, music, canceled
+	playing = False
+	if (music != None):
+		music.terminate()
+		music = None
+		if (canceled == True):
+			printInline("Playback canceled.")
+		else:
+			printInline("Awaiting next poopist...")
+
+#Before we exit, stop the music if it's still playing
+def clean(*args):
+	StopMusic()
+	sys.exit(0)
+
+#Run our 'clean' function if the script is interrupted
+for sig in (SIGABRT, SIGILL, SIGINT, SIGSEGV, SIGTERM):
+	signal(sig, clean)
+
+if (args.lighting == True):
+	#Only print out the photo resistor reading
+	while True:
+		printInline("Light level: " + str(RCtime(LIGHT_SENSOR_PIN)))
+
+print ""
+print ""
+print "PoopTunes 0.0.1 (alpha)"
+print ""
+print ""
+
+print("                   ______                             ")
+print("                   \     \.                           ")
+print("                   |`\_____\               ____------ ")
+print("                   |` |    |              ||       || ")
+print("                   |  |    |              ||       || ")
+print("      __-====-__  _|  |    |              ||       || ")
+print("     (~<       >~>  \ |    |              ||      (#) ")
+print("     !~~-====-~~/----`+----/            (#)           ")
+print("      \         \___     /                            ")
+print("       >------\     \  <                              ")
+print("      <_________________>                             ")
+
+
+print ""
+print ""
+
+printInline("Awaiting first poopist...")
+
+#The main loop
+while True:
+
+	#Set flags for button state
+	if (GPIO.input(BUTTON_PIN) == False):
+		buttonDown = True
+	else:
+		buttonDown = False
+		#Debouncing with buttonEventFired to see if we've handled the button
+		buttonEventFired = False
+	if (buttonDown and buttonEventFired == False):
+		buttonEventFired = True
+		#The button stops the song
+		if (playing == True and music != None):
+			canceled = True
+			StopMusic()
+
+	#See if the light is on or not
+	lightOn = RCtime(LIGHT_SENSOR_PIN) < threshold
+
+	#Only start a song if the light is on and one isn't currently playing. And make sure we didn't just hit the cancel button
+	if (playing == False and lightOn == True and canceled == False):
+		playing = True
+		#Get a random song
+		song = GetSong()
+		#Start the song
+		music = Popen(['mpg123', "-q", songPath + song])
+		poopistCount += 1
+		hasPlayedOnce = True
+		printInline("Playing: " + song)
+	elif (playing == True and lightOn == False and music != None):
+		#Lights turned off and music is playing; stop the music!
+		StopMusic()
+	if (lightOn == False):
+		#If the light is off, set the canceled flag to False to reset it so we can cancel again next time.
+		canceled = False
+		if (hasPlayedOnce == True):
+			#Check if it's played once so we don't overwrite the initial message
+			printInline("Awaiting next poopist...")
+
